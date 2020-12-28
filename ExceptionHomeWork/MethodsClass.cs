bdffcfd5cc5952dcdd58a93f22d3b66d615e3032
Bolsyn <Serikagis@yandex.ru>
2020-12-28 21:11:21 +0600

    Add project files.

---
+++ ExceptionHomeWork/MethodsClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionHomeWork
+{
+    class MethodsClass
+    {
+        public double DivNumbers(double number, double anotherNumber)
+        {
+            if (number == 0 || anotherNumber == 0)
+            {
+                throw new ArgumentException("Argument equal zero");
+            }
+            else if (number == null || anotherNumber == null)
+            {
+                throw new ArgumentNullException("Argument are/is null ");
+            }
+            else
+            {
+                return number / anotherNumber;
+            }
+        }
+    }
+}
---
+++ ExceptionHomeWork/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ExceptionHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5 };
+            try
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    array[i] = i;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("We have error");
+            }
+            finally
+            {
+                Console.WriteLine("Work with array done");
+            }
+            foreach (var el in array)
+            {
+                Console.WriteLine(el);
+            }
+        }
+    }
+}
